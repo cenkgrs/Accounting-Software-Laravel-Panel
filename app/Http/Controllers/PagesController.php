38c9e13e90a1a0108e31be6e3d846d1f699bb53d
cenkgrs <cenkgrs@gmail.com>
2020-10-06 16:03:11 +0300

    Dashboard page stocks panel created Showcase blocks css prepared

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -7,6 +7,14 @@ use Illuminate\Http\Request;
 class PagesController extends Controller
 {
     public function dashboard(){
-        return view('home.index');
+        $data["stocks"] = [
+            "fruit"         => ["name" => "Fruit Stock", "quantity" => 50, "unit" => "kg"],
+            "drink"         => ["name" => "Drink Stock", "quantity" => 10, "unit" => "lt"],
+            "meal"          => ["name" => "Meal Stock", "quantity" => 95, "unit" => "piece"],
+            "vegetables"    => ["name" => "Vegetable Stock", "quantity" => 10, "unit" => "kg"],
+        ];
+
+
+        return view('home.index', $data);
     }
 }
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,5 +1,28 @@
 @extends('index')
 
 @section('content')
+
+<div class="dashboard">
+    <div class="dashboard-header">Dashboard</div>
+    <div class="dashboard-body">
+        <div class="row">
+            <div class="col-lg-12 col-xs-12 dashboard-sub-header">
+                Stocks
+            </div>
+            @foreach($stocks as $stock)
+                <div class="col-lg-3 col-xs-3">
+                    <div class="showcase">
+                        <div class="showcase-header">
+                            {{ $stock['name'] }}
+                        </div>
+                        <div class="showcase-body">
+                            {{ $stock['quantity'] }} {{ $stock['unit'] }}
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+</div>
     
 @endsection
